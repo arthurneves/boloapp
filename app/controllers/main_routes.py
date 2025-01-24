22f998c66ad96796ff55e83e3bea9b4d1085f543
arthurneves <arthur.neves@live.com>
2025-01-23 22:56:50 -0300

    fix: correção de bug no cache da home

--- app/controllers/main_routes.py
+++ app/controllers/main_routes.py
@@ -1,12 +1,13 @@
 from flask_login import login_required, current_user
 from . import main_bp, usuario_routes
+from app.services.cache_service import cache_perfil_home
 
 
 @main_bp.route('/')
 @login_required
+@cache_perfil_home()
 def home():
 
     id_usuario = current_user.id_usuario
 
     return usuario_routes.perfil_usuario(id_usuario)
-
